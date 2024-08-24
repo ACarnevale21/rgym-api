858d971b84a167c09876e39b80ef85befb95d685
AgustinCarnvale <carnevaleagustinj@gmail.com>
2024-08-24 16:01:25 -0300

    refactor update user request dto

--- src/modules/user/application/dto/request/update-user.dto.ts
+++ src/modules/user/application/dto/request/update-user.dto.ts
@@ -1,9 +1,11 @@
-import { Types } from 'mongoose';
+import { UserGoal } from '../../enum/user-goal.enum';
 
 export class UpdateUserRequestDto {
   name?: string;
   email?: string;
   password?: string;
-  description?: string;
-  routine?: Types.ObjectId[];
+  age?: number;
+  weight?: number;
+  height?: number;
+  goal?: UserGoal;
 }
